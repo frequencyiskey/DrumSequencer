a42809b16aa380dc8ac182ad6bcde165bdf8f90d
Gabriella Watkins <gabbleswat@gmail.com>
2018-09-12 14:21:21 +0100

    Initial commit, created basic drum map

---
+++ Sequencer/app/src/main/java/com/backscratch/gabriella/sequencer/SequencerActivity.java
@@ -0,0 +1,50 @@
+package com.backscratch.gabriella.sequencer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SequencerActivity extends AppCompatActivity {
+
+    public MediaPlayer kickWav, snareWav, hihatWav, highTomWav,
+            midTomWav, lowTomWav, crashWav, clapWav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sequencer);
+
+        kickWav = MediaPlayer.create(this, R.raw.kick);
+        snareWav = MediaPlayer.create(this, R.raw.snare3);
+        hihatWav = MediaPlayer.create(this, R.raw.hihat2);
+        highTomWav = MediaPlayer.create(this, R.raw.high_tom);
+        midTomWav = MediaPlayer.create(this, R.raw.mid_tom);
+        lowTomWav = MediaPlayer.create(this, R.raw.low_tom);
+        crashWav = MediaPlayer.create(this, R.raw.crash);
+        clapWav = MediaPlayer.create(this, R.raw.clap);
+    }
+
+    public void onSeqClick(View view) {
+        String btnTag = "";
+        btnTag = (String) view.getTag();
+
+        if (btnTag.contains("kick")) {
+            kickWav.start();
+        } else if (btnTag.contains("snare")) {
+            snareWav.start();
+        } else if (btnTag.contains("hihat")) {
+            hihatWav.start();
+        } else if (btnTag.contains("highTom")) {
+            highTomWav.start();
+        } else if (btnTag.contains("midTom")) {
+            midTomWav.start();
+        } else if (btnTag.contains("lowTom")) {
+            lowTomWav.start();
+        } else if (btnTag.contains("crash")) {
+            crashWav.start();
+        } else if (btnTag.contains("clap")) {
+            clapWav.start();
+        }
+    }
+}
